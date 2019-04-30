8fffe56b10d52e1e2f27bdc6172a92e4637d5c54
ernestogl88 <ernestogomezlopez@gmail.com>
2019-04-30 13:48:17 +0200

    Coolbutton changed by object access to all properties

--- starter-code/src/CoolButton.js
+++ starter-code/src/CoolButton.js
@@ -1,15 +1,45 @@
-import React from 'react'
+import React from "react";
 
 export default class CoolButton extends React.Component {
-  render() {
-    let classes = this.props.className
-    if (this.props.isSmall != undefined) classes += ' is-small';
-    if (this.props.isDanger != undefined) classes += ' is-danger';
-    if (this.props.isSuccess != undefined) classes += ' is-success';
-    classes += ' button'
+  constructor(props) {
+    super(props)
+    this.className = {
+      isActive: "is-active",
+      isBlack: "is-black",
+      isCentered: "is-centered",
+      isDanger: "is-danger",
+      isDark: "is-dark",
+      isFocused: "is-focused",
+      isGrouped: "is-grouped",
+      isHovered: "is-hovered",
+      isInfo: "is-info",
+      isInverted: "is-inverted",
+      isLarge: "is-large",
+      isLight: "is-light",
+      isLink: "is-link",
+      isLoading: "is-loading",
+      isMedium: "is-medium",
+      isOutlined: "is-outlined",
+      isPrimary: "is-primary",
+      isRight: "is-right",
+      isRounded: "is-rounded",
+      isSelected: "is-selected",
+      isSmall: "is-small",
+      isStatic: "is-static",
+      isSuccess: "is-success",
+      isText: "is-text",
+      isWarning: "is-warning",
+      isWhite: "is-white"
+    };
+    this.classes =
+      "button " +
+      Object.keys(this.props)
+        .map(element => this.className[element])
+        .concat(this.props.className)
+        .join(" ");
+  }
 
-    return (
-      <button className={classes}>{this.props.children}</button>
-    )
+  render() {
+    return <button className={this.classes}>{this.props.children}</button>;
   }
 }
